b2efbdb2b367b855e79d1080d5616946fb5815be
Igor Ovsyannikov <kamish@outlook.com>
2019-09-26 14:42:37 +0300

    Removed examples from the 1st version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,9 @@
-import atexit
-
-import cleo
-import click.testing
 import pytest
 import plac
 
 import todo_argparse
-import todo_cement
-import todo_cleo
-import todo_click
 import todo_cliff
 import todo_cmd2
-import todo_docopt
-import todo_fire
 import todo_plac
 import todo_plumbum
 import todolib
@@ -46,41 +37,6 @@ def test_argparse(capsys):
     assert out == EXPECTED
 
 
-def test_docopt(capsys):
-    todo_docopt.main(["add", "test"])
-    out, _ = capsys.readouterr()
-    assert out == EXPECTED
-
-
-def test_click():
-    runner = click.testing.CliRunner()
-    result = runner.invoke(todo_click.cli, ["add", "test"])
-    assert result.exit_code == 0
-    assert result.output == EXPECTED
-
-
-def test_fire(capsys):
-    todo_fire.main(["add", "test"])
-    out, _ = capsys.readouterr()
-    assert out == EXPECTED
-
-
-def test_cement(capsys):
-    with todo_cement.TodoApp(argv=["add", "test"]) as app:
-        app.run()
-        out, _ = capsys.readouterr()
-        assert out == EXPECTED
-        # for asserting jinja output, not used in this application
-        assert app.last_rendered is None
-
-
-def test_cleo():
-    app = todo_cleo.TodoApp()
-    command = app.find("add")
-    tester = cleo.CommandTester(command)
-    tester.execute("test")
-    assert tester.status_code == 0
-    assert tester.io.fetch_output() == "Task test created with number 0.\n"
 
 
 def test_cliff(capsys):
